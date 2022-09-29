50991d9c7f5afba8b64d3e567fbe15a8186c8bd3
gilbert <s3665887@student.rmit.edu.au>
2022-09-29 23:10:15 +1000

    inplemented goal_check

--- goal_check.asm
+++ goal_check.asm
@@ -1,6 +1,30 @@
 .ORIG x3000
+LD R3, GOAL_X
+NOT R3, R3
+ADD R3, R3 #1
+
+LD R4, GOAL_Z
+NOT R4, R4
+ADD R4, R4 #1
+
+LOOP_
+TRAP 0x32
+ADD R0, R0, R3
+TRAP 0x30
+BRnp LOOP_
+
+ADD R2, R2, R4
+BRnp LOOP_
+
+LEA R0 STRING
+TRAP 0x31
+
 HALT
 ; Note: Please do not change the names of the constants below
-GOAL_X .FILL #4
-GOAL_Z .FILL #-5
+;prepare for subtraction
+; GOAL_X .FILL #4
+; GOAL_Z .FILL #-5
+GOAL_X .FILL #10
+GOAL_Z .FILL #1
+STRING .STRINGZ	"goal reached"
 .END
