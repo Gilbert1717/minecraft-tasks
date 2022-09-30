4bb8fbe502e200bc17e826e3785ac301c0640ea6
gilbert <s3665887@student.rmit.edu.au>
2022-09-30 17:44:22 +1000

    implement lamps following the requirement implement euclidean_dist

--- euclidean_dist.asm
+++ euclidean_dist.asm
@@ -1,8 +1,65 @@
 .ORIG x3000
+TRAP 0x32
+LD, R3 G_X
+NOT R3, R3
+ADD R3, R3, #1
+ADD R5, R0, R3
+JSR Square
+ADD R0, R3, #0
+
+LD, R3 G_Y
+NOT R3, R3
+ADD R3, R3, #1
+ADD R5, R1, R3
+JSR Square
+ADD R1, R3, #0
+
+LD, R3 G_Z 
+NOT R3, R3
+ADD R3, R3, #1
+ADD R5, R2, R3
+JSR Square
+ADD R2, R3, #0
+
+;sum (x - G_X)^2 , (y - G_Y)^2, (z - G_Z)^2 up
+ADD R0, R0, R1
+ADD R0, R0, R2
+
+LD, R5 GOAL_DIST
+JSR Square
+NOT R3, R3
+ADD R3, R3, #1
+ADD R0, R0, R3
+BRn Reach_
+
+LEA	R0,	OUT_DIST
+JSR Print_
+
+Reach_
+    LEA	R0,	IN_DIST
+
+Print_
+TRAP 0x31
+
 HALT
+;R3 and R4 for the result and loop number R5 for the coord-number
+Square
+        ADD R3 R5 #0
+        TRAP 0x30
+        ADD R4 R5 #-1
+        LOOP
+        ADD R3 R3 R5
+        ADD R4 R4 #-1
+        BRp LOOP
+        RET
+
 ; Note: Please do not change the names of the constants below
 G_X .FILL #7
 G_Y .FILL #-8
 G_Z .FILL #5
 GOAL_DIST .FILL #10
+
+
+IN_DIST .STRINGZ	"The player is within distance of the goal"
+OUT_DIST .STRINGZ	"The player is outside the goal bounds"
 .END
--- lamps.asm
+++ lamps.asm
@@ -1,34 +1,37 @@
 .ORIG x3000
-
 TRAP 0x32
 ADD R0 R0 #2
+ADD R5 R1 #0
 ADD R2 R2 #2
-ADD R3 R3 #1
-LD, R4 STONE_level
-Lamp_Base
-    JSR Lamp
-    ADD R4 R4 #-1
-BRp Lamp_Base
-LD, R3 Glow
 JSR Lamp
+ADD R1 R5 #0
+ADD R2 R2 #-4
+JSR Lamp
+ADD R1 R5 #0
+ADD R0 R0 #-4
+JSR Lamp
+ADD R1 R5 #0
+ADD R2 R2 #4
+JSR Lamp
+
 
 
 
 HALT
 Lamp    
+    LD, R3 Stone
+    ADD R4 R4 #3
+    Lamp_Base
+        TRAP 0x35
+        ADD R1 R1 #1
+        ADD R4 R4 #-1
+    BRp Lamp_Base
+    LD, R3 Glow
     TRAP 0x35
-    ADD R2 R2 #-4
-    TRAP 0x35
-    ADD R0 R0 #-4
-    TRAP 0x35
-    ADD R2 R2 #4
-    TRAP 0x35
-    ADD R0 R0 #4
-    ADD R1 R1 #1
     RET
-STONE_level .FILL #3
 Glow .FILL	#89
-CL .FILL Lamp
+Stone .FILL	#1
+
 
 
 .END
