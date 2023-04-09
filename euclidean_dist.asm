64144c3f67fb1fbb7e59543a966b970876a5d2a9
gilbert <duyiqi1717@gmail.com>
2023-04-09 19:10:45 +1000

    Update gitignore.

--- euclidean_dist.asm
+++ euclidean_dist.asm
@@ -1,5 +1,7 @@
 .ORIG x3000
-TRAP 0x32
+LD, R0 X_
+LD, R1 Y_
+LD, R2 Z_
 LD, R3 G_X
 NOT R3, R3
 ADD R3, R3, #1
@@ -29,7 +31,7 @@ LD, R5 GOAL_DIST
 JSR Square
 NOT R3, R3
 ADD R3, R3, #1
-ADD R0, R0, R3
+ADD R0, R5, R3
 BRn Reach_
 
 LEA	R0,	OUT_DIST
@@ -39,13 +41,13 @@ Reach_
     LEA	R0,	IN_DIST
 
 Print_
+TRAP 0x30
 TRAP 0x31
 
 HALT
 ;R3 and R4 for the result and loop number R5 for the coord-number
 Square
         ADD R3 R5 #0
-        TRAP 0x30
         ADD R4 R5 #-1
         LOOP
         ADD R3 R3 R5
@@ -54,10 +56,14 @@ Square
         RET
 
 ; Note: Please do not change the names of the constants below
-G_X .FILL #7
-G_Y .FILL #-8
-G_Z .FILL #5
-GOAL_DIST .FILL #10
+X_ .FILL #15
+Y_ .FILL	#32
+Z_ .FILL	#-22
+
+G_X .FILL #5
+G_Y .FILL #10
+G_Z .FILL #4
+GOAL_DIST .FILL #35
 
 
 IN_DIST .STRINGZ	"The player is within distance of the goal"
